96ac607265ce84663c4a61a9f40fadee95f3a7e1
Marciano Schildmeijer <marciano@kabisa.nl>
2025-01-12 11:49:08 +0100

    Improve mixed calc

--- src/Cell/Cell.tsx
+++ src/Cell/Cell.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { MAX_WIDTH } from "../Container";
 import { LevelConfig, useGameState } from "../useGameState";
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
 import { Character, CHARACTERS, FACE_SIZE } from "../constants";
 
 type CellProps = {
@@ -12,28 +12,27 @@ type CellProps = {
 const Cell = ({ cell, onClick }: CellProps) => {
 	const { gameState } = useGameState();
 
-	const {
-		config: { type },
-	} = gameState as { config: LevelConfig };
-
-	const randomPosition = useRef({
-		top: `${Math.floor(Math.random() * (MAX_WIDTH - FACE_SIZE))}px`,
-		left: `${Math.floor(Math.random() * (MAX_WIDTH - FACE_SIZE))}px`,
-	});
+	const randomPosition = useMemo(
+		() => ({
+			top: `${Math.floor(Math.random() * (MAX_WIDTH - FACE_SIZE))}px`,
+			left: `${Math.floor(Math.random() * (MAX_WIDTH - FACE_SIZE))}px`,
+		}),
+		[]
+	);
 
 	const isTarget = cell === gameState.config.target;
+
+	const cellClasses = clsx("cell", "cursor-pointer", "size-12", {
+		mixed: gameState.config.type.mixed,
+	});
 
 	return (
 		<div
 			onClick={onClick}
 			data-cell-color={cell}
-			className={clsx("cell", "cursor-pointer", "size-12", {
-				mixed: type.mixed ? randomPosition.current : undefined,
-			})}
+			className={cellClasses}
 			data-is-target={isTarget ? "target" : undefined}
-			style={{
-				...randomPosition.current,
-			}}
+			style={randomPosition}
 		>
 			<img src={CHARACTERS[cell]} alt={cell} className="w-full" />
 		</div>
