08849569cc8bf6162552d23688924cadded5632a
Marciano Schildmeijer <marciano@kabisa.nl>
2025-01-12 14:05:56 +0100

    Add level to gameover screen.

--- src/GameOver/GameOver.tsx
+++ src/GameOver/GameOver.tsx
@@ -6,6 +6,7 @@ const GameOver = () => {
 		<div>
 			<h1>Game Over</h1>
 			<p>Your score: {gameState.score}</p>
+			<p>Total level: {gameState.currentLevel} </p>
 			<p>Try again?</p>
 			<button onClick={restartGame}>Restart</button>
 		</div>
